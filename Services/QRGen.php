16324354831276a830e48f929c514b309aa5db87
Savin Mikhail <mikhail.d.savin@gmail.com>
2023-11-09 18:31:37 +0700

    feat(qr): create service to reuse logic

---
+++ Services/QRGen.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\Services;
+
+use Endroid\QrCode\Color\Color;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelLow;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\SvgWriter;
+
+class QRGen
+{
+    public static function renderQR(string $data): string
+    {
+        $writer = new SvgWriter();
+
+        // Create QR code
+        $qrCode = QrCode::create($data)
+            ->setEncoding(new Encoding('UTF-8'))
+            ->setErrorCorrectionLevel(new ErrorCorrectionLevelLow())
+            ->setSize(300)
+            ->setMargin(10)
+            ->setRoundBlockSizeMode(new RoundBlockSizeModeMargin())
+            ->setForegroundColor(new Color(0, 0, 0))
+            ->setBackgroundColor(new Color(255, 255, 255));
+
+        $result = $writer->write($qrCode);
+
+        $svg = $result->getString();
+        return $svg;
+    }
+}
