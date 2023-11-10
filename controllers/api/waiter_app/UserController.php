d2b5dab574ea5b3bef8dbf1502ba1dd55c1988a7
Savin Mikhail <mikhail.d.savin@gmail.com>
2023-11-10 11:36:57 +0700

    feat(user): login, check-auth

--- controllers/api/waiter_app/UserController.php
+++ controllers/api/waiter_app/UserController.php
@@ -9,6 +9,9 @@ use yii\filters\AccessControl;
 
 class UserController extends ApiController
 {
+    // Отключение CSRF валидации для API запросов
+    public $enableCsrfValidation = false;
+
     public function behaviors()
     {
         return [
@@ -18,20 +21,18 @@ class UserController extends ApiController
                 'rules' => [
                     [
                         'allow' => true,
-                        'actions' => ['index', 'tariffs', 'logout', 'remove', 'update', 'notifications', 'orders', 'feedback', 'tariffs', 'notificationscnt'],
+                        'actions' => ['index', 'check-auth', 'logout',],
                         'roles' => ['@'],
                     ],
                     [
                         'allow' => true,
-                        'actions' => ['registration', 'fastregister'],
+                        'actions' => ['login'],
                         'roles' => ['?'],
                     ],
                 ],
                 'denyCallback' => function ($rule, $action) {
-                    if ($action->id == 'registration') {
-                        return $this->asJson(['result' => false, 'errors' => ['user' => ['message' => 'user logged in', 'code' => '403']]]);
-                    } else {
-                        return $this->asJson(['result' => false, 'errors' => ['user' => ['message' => 'authorization required', 'code' => '401']]]);
+                    if ($action->id !== 'login') {
+                        $this->sendResponse(401, ['data' => 'authorization required']);
                     }
                 },
             ],
@@ -39,8 +40,8 @@ class UserController extends ApiController
     }
 
     /**
-     * @SWG\Get(path="/api/user",
-     *     tags={"User"},
+     * @SWG\Get(path="/api/waiter_app/user",
+     *     tags={"WaiterApp\User"},
      *     @SWG\Response(
      *         response = 200,
      *         description = "User collection response",
@@ -52,12 +53,12 @@ class UserController extends ApiController
     {
         $obUser = User::findIdentity(Yii::$app->user->identity->getId());
 
-        return $this->asJson(['success' => true, 'user' => $obUser]);
+        $this->sendResponse(200, ['user' => $obUser]);
     }
 
     /**
-     * @SWG\Get(path="/api/user/logout",
-     *     tags={"User"},
+     * @SWG\Get(path="/api/waiter_app/user/logout",
+     *     tags={"WaiterApp\User"},
      *     @SWG\Response(
      *         response = 200,
      *         description = "User collection response",
@@ -70,5 +71,62 @@ class UserController extends ApiController
         Yii::$app->user->logout();
 
         return $this->asJson(['result' => 1]);
+    }
+
+    /**
+     * @SWG\Post(path="/api/waiter_app/user/login",
+     *     tags={"WaiterApp\User"},
+     *      @SWG\Parameter(
+     *          name="login",
+     *          in="formData",
+     *          type="string"
+     *      ),
+     *      @SWG\Parameter(
+     *          name="password",
+     *          in="formData",
+     *          type="string"
+     *      ),
+     *     @SWG\Response(
+     *         response = 200,
+     *         description = "User login response",
+     *         @SWG\Schema(ref = "#/definitions/Products")
+     *     ),
+     * )
+     */
+    public function actionLogin()
+    {
+        $username = Yii::$app->request->post('login');
+        $password = Yii::$app->request->post('password');
+
+        if(!$username || !$password) {
+            $this->sendResponse(401, ['data' => 'Получены невалидные данные']);
+        }
+        // Поиск пользователя по имени пользователя
+        $user = User::findByUsername($username);
+
+        if (!$user || !$user->validatePassword($password)) {
+            $this->sendResponse(401, ['data' => 'Неверный логин или пароль']);
+        }
+
+        // Аутентификация пользователя
+        Yii::$app->user->login($user);
+
+        $this->sendResponse(200, ['data' => 'Аутентификация прошла успешно']);
+    }
+
+    /**
+     * @SWG\Get(path="/api/waiter_app/user/check-auth",
+     *     tags={"WaiterApp\User"},
+     *     @SWG\Response(
+     *         response = 200,
+     *         description = "return true if user authorized, else return false",
+     *         @SWG\Schema(ref = "#/definitions/Products")
+     *     ),
+     * )
+     */
+    public function actionCheckAuth()
+    {
+        $isUserAuthorized = Yii::$app->user->isGuest;
+        $this->sendResponse(200, ['data' => $isUserAuthorized]);
     }
 }
