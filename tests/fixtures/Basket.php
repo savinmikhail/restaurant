9fd996bcc5c92d701f7f349e520df1ca52a901e8
Savin Mikhail <mikhail.d.savin@gmail.com>
2023-12-12 08:59:45 +0700

    feat(test): trying to setup fixtures

---
+++ tests/fixtures/Basket.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace app\tests\fixtures;
+
+class BasketFixture extends \yii\test\ActiveFixture
+{
+    public $modelClass = 'app\models\tables\Basket';
+}
---
+++ tests/fixtures/data/Basket.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'basket1' => [
+        'id'  => 1,
+        'table_id'  => 1,
+        'created_at'  => '2020-01-01 00:00:00',
+        'updated_at'   => '2020-01-01 00:00:00',
+        'basket_total' => 100,
+
+    ],
+];
